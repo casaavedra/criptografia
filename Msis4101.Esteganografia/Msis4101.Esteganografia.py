d03f92129405becf686810a5daa1e462633957c6
CarlosS <ca.saaavedra@uniandes.edu.co>
2017-02-28 17:11:27 -0500

    Priemer avance

--- Msis4101.Esteganografia/Msis4101.Esteganografia.py
+++ Msis4101.Esteganografia/Msis4101.Esteganografia.py
@@ -1,31 +1,34 @@
-from mido import MidiFile, Message
+from mido import MidiFile, Message, MidiTrack
 
 mid = MidiFile('song.mid')
 mid_new = MidiFile()
+mid_new.tracks.append(mid.tracks)
 
 mystring = 'HOLAMUNDO'
+letter_count = 0
 b = mystring.encode('utf-8')
 
-for i, track in enumerate(mid):
-    mid_new.tracks.append(track)
+for i, track in enumerate(mid):    
     count_pc = 0
-    if track.type == 'program_change':
-        new_program = bytes([b[count_pc]]) + bytes([track.program])
-        int_new_program = int.from_bytes(new_program, byteorder='big')
-        print('Track {}: {}, {}'.format(i, track.type, track.program))    
-        print('Track {}: {}, {}'.format(i, track.type, int_new_program))
-        track.program = int_new_program
+    if track.type == 'program_change':       
+        letter = mystring[letter_count] 
+        new_program = int.from_bytes(letter.encode('utf-8'), byteorder='big')
+        new_program_l = new_program & 0x0f
+        new_program_h = (new_program & 0xf0) >> 4
+        track_new = MidiTrack()   
+        mid_new.tracks.append(track_new)     
+        mid_new.tracks.append(Message('program_change', program=128, time=0))
+        mid_new.tracks.append(Message('program_change', program=new_program_h, time=0))
+        print('Track {}: channel {}, program {}'.format(i, track.channel, hex(track.program)))    
         count_pc +1
+    mid_new.tracks.append(track)
 
-mid.save('song3_copy.mid')
+mid_new.save('song3_copy.mid')
 
 
-split_letters = []
-for letter in b:
-    hex_letter = hex(letter)
-    hi_hex_letter = hex_letter & 0x0F
-    split_letters.append(letter[:3])
-    split_letters.append(letter[3:])
+
+
+
 
 
         
